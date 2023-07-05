32c4a67e25e6ec65fa66c179afea372c0c14f484
Jim Ezesinachi <ezesinachijim@gmail.com>
2023-07-05 13:19:59 +0100

    feat: completed
    Signed-off-by: Jim Ezesinachi <ezesinachijim@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::{env, process};
+
+use minigrep::{run, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for `{}`,", config.query);
+    println!("In file '{}':\n", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
